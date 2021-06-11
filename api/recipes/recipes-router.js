348ca8898e6983cd438180cdb27298c60f9863a3
drslaughterrex <drslaughterrex@gmail.com>
2021-06-10 21:21:08 -0500

    MVP

---
+++ api/recipes/recipes-router.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const recipe = require('./recipes-model')
+
+router.get('/:recipes_id', (req, res, next) => {
+    Recipe.getRecipeById(req.params.recipe_id)
+    .then(resource => {
+        res.status(200).json(resource)
+    })
+    .catch(next)
+})
+
+
+
+
+router.use((err, req, res, next) => {
+	res.status(500).json({
+		customMessage: "something went wrong inside the recipes router",
+		message: err.message,
+		stack: err.stack,
+	});
+});
+
+module.exports = router;
